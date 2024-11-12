ee5a3d318b57ef114cf5a76e8edd7a3c27288150
youngboypimpin <rjtekdom@gmail.com>
2024-11-12 16:29:27 +0100

    create root page and layout

--- app/create/layout.tsx
+++ app/create/layout.tsx
@@ -1,24 +1,44 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+import { ThemeProvider } from 'next-themes';
+import localFont from "next/font/local";
+import "../globals.css";
 import CreateSegmentControl from "../components/create_segmented_control";
+import Drawer from "../components/drawer";
 
-
+const geistSans = localFont({
+	src: "../fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
+});
+const geistMono = localFont({
+	src: "../fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
+});
 
 export const metadata: Metadata = {
 	title: "Invoice Automation Service",
 	description: "Created by Patrick Aigbogun",
 };
 
-
-export default function CreateLayout({
+export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
-}>){
-<html>
-    <body>
-        <CreateSegmentControl />
-        {children}
-    </body>
-</html>
+}>) {
+	return (
+		<section>
+			<Drawer />
+			<section
+				className={`${geistSans.variable} ${geistMono.variable} antialiased text-white mx-auto w-[95%] sm:w-[75%] `}
+			>
+				<CreateSegmentControl />
+				{children}
+			</section>
 
+		</section>
+
+	);
 }
