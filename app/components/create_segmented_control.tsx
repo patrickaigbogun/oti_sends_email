9e5284ac6c6f9ed9a8b8eb9fd663463b0aba266a
youngboypimpin <rjtekdom@gmail.com>
2024-11-12 16:29:56 +0100

    segment control component for switching view easily

--- app/components/create_segmented_control.tsx
+++ app/components/create_segmented_control.tsx
@@ -1,20 +1,39 @@
+'use client';
+
+import { ClockCounterClockwise, House } from "@phosphor-icons/react/dist/ssr";
 import { Flex, SegmentedControl } from "@radix-ui/themes";
 import Link from "next/link";
 
 export default function CreateSegmentControl() {
-	return(
-		<Flex align="center" direction="row" gap="4">
-		<SegmentedControl.Root defaultValue="inbox" radius="full">
-			<Link href={"/create/invoice"}>
-				<SegmentedControl.Item value="inbox">Invoice</SegmentedControl.Item>
-			</Link>
-			<Link href={"/create/customers"}>
-				<SegmentedControl.Item value="drafts">Customers</SegmentedControl.Item>
-			</Link>
-			<Link href={"/create/reciepts"}>
-				<SegmentedControl.Item value="sent">Reciepts</SegmentedControl.Item>
-			</Link>
-		</SegmentedControl.Root>
-	</Flex>
+	return (
+		<Flex align="center" direction="row" gap="4" justify='center' >
+			<SegmentedControl.Root radius="full">
+				<SegmentedControl.Item value="Home">
+					<Link href={'/create'} >
+						<House size={32} />
+					</Link>
+				</SegmentedControl.Item>
+				<SegmentedControl.Item value="Invoice">
+					<Link href={"/create/invoice"}>
+						Invoice
+					</Link>
+				</SegmentedControl.Item>
+				<SegmentedControl.Item value="Customers">
+					<Link href={"/create/customers"}>
+						Customers
+					</Link>
+				</SegmentedControl.Item>
+				<SegmentedControl.Item value="Reciepts">
+					<Link href={"/create/reciepts"}>
+						Reciepts
+					</Link>
+				</SegmentedControl.Item>
+				<SegmentedControl.Item value="Recents">
+					<Link href={'/create/recents'} >
+						<ClockCounterClockwise size={32} />
+					</Link>
+				</SegmentedControl.Item>
+			</SegmentedControl.Root>
+		</Flex>
 	);
 }
