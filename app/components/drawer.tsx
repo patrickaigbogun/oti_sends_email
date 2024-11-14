fd4764ccfece7fcdcfa058630f3988a4dedf7584
youngboypimpin <rjtekdom@gmail.com>
2024-11-14 17:19:37 +0100

    (fix): bug with z-index

--- app/components/drawer.tsx
+++ app/components/drawer.tsx
@@ -11,9 +11,9 @@ export default function Drawer() {
 	};
 
 	return (
-		<div className="relative">
+		<div className="relative z-10">
 			<button
-			className={` transition-all ${isOpen? 'invisible': 'visible'}`}
+				className={` transition-all ${isOpen ? 'invisible' : 'visible'}`}
 				onClick={toggleDrawer}
 			>
 				<DotsThreeCircle size={32} />
@@ -22,14 +22,14 @@ export default function Drawer() {
 				className={`fixed top-0 bg-black bg-opacity-40 backdrop-blur-lg shadow-lg left-0 h-full w-64 p-4 transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'
 					}`}
 			>
-			<span className='flex flex-row justify-between' >
-			<h3 className="text-lg font-medium mb-2">Drawer Content</h3>
-				<button
-				onClick={toggleDrawer}
-			>
-				<DotsThreeCircle size={32} />
-			</button>
-			</span>
+				<span className='flex flex-row justify-between' >
+					<h3 className="text-lg font-medium mb-2">Drawer Content</h3>
+					<button
+						onClick={toggleDrawer}
+					>
+						<DotsThreeCircle size={32} />
+					</button>
+				</span>
 				<p>This is the content that will be displayed in the drawer.</p>
 			</div>
 		</div>
