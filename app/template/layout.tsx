8ee5270f955e91d3ef5eaa689a1544bc3298fe39
youngboypimpin <rjtekdom@gmail.com>
2024-11-19 00:41:47 +0100

    cleaned up code

--- app/template/layout.tsx
+++ app/template/layout.tsx
@@ -1,26 +1,9 @@
 import type { Metadata } from "next";
-import '@radix-ui/themes/styles.css';
-import { Theme } from '@radix-ui/themes';
-import { ThemeProvider } from 'next-themes';
-import localFont from "next/font/local";
-import "../globals.css";
-import CreateSegmentControl from "../components/create_segmented_control";
-import Drawer from "../components/drawer";
 import CreateLayoutHeader from "../components/create_layout_header";
 
-const geistSans = localFont({
-	src: "../fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
-});
-const geistMono = localFont({
-	src: "../fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
-});
 
 export const metadata: Metadata = {
-	title: "Invoice Automation Service",
+	title: "Create your templates",
 	description: "Created by Patrick Aigbogun",
 };
 
@@ -33,7 +16,6 @@ export default function RootLayout({
 		<section className='w-full'>
 			<CreateLayoutHeader />
 			<section>
-				{/* <CreateSegmentControl /> */}
 				{children}
 			</section>
 
