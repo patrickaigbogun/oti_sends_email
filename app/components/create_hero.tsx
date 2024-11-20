59fa05ae9dbb94c84ace31605216f209a609fa91
youngboypimpin <rjtekdom@gmail.com>
2024-11-20 19:20:24 +0100

    updated UI for template hero section

--- app/components/create_hero.tsx
+++ app/components/create_hero.tsx
@@ -1,26 +1,62 @@
 'use client';
 
-
+import { baseUrl } from "@/utils/constants";
 import { PlusCircle } from "@phosphor-icons/react";
-import { Heading } from "@radix-ui/themes";
+import { Heading, Text, Container, Flex, Box, Strong } from "@radix-ui/themes";
 import Link from "next/link";
 
-
 export default function CreateHero() {
-    return (
-        <section className="space-y-8" >
-            <Heading>Here you can choose to create your customers, invoices, reciepts, etc</Heading>
-            <section className="h-fit grid grid-cols-3 gap-4">
-                <Link className="bg-gradient-to-br from-teal-400 to-gray-800 flex object-cover h-48 items-center justify-center rounded-xl brightness-75" href={''}>
-                    <PlusCircle size={56} />
-                </Link>
-                <Link className="bg-gradient-to-br from-violet-500 to-orange-300 flex object-cover h-48 items-center justify-center rounded-xl brightness-75" href={''}>
-                    <PlusCircle size={56} />
-                </Link>
-                <Link className="bg-gradient-to-br from-neutral-600 via-orange-200 flex to-stone-700 object-cover h-48 items-center justify-center rounded-xl brightness-75" href={''}>
-                    <PlusCircle size={56} />
-                </Link>
-            </section>
-        </section>
-    );
+	return (
+		<Container className="py-12 md:py-24">
+			<Flex direction="column" gap="6">
+				<Box>
+					<Heading size="8" className="mb-4">Create New Templates</Heading>
+					<Text size="5" className="text-gray-500">
+						Here you can choose to create your customers, invoices, receipts, etc
+					</Text>
+				</Box>
+				<Flex gap="4" wrap="wrap">
+					<Link href={`${baseUrl}/create/customer`} className="flex-1 min-w-[250px]">
+						<Box className="bg-gradient-to-br from-teal-400 to-gray-800 h-48 rounded-xl overflow-hidden group">
+							<Flex
+								align="center"
+								justify="center"
+								className="w-full h-full bg-black bg-opacity-25 group-hover:bg-opacity-0 transition-all duration-300"
+							>
+								<span className="flex flex-col items-center" >
+									<PlusCircle size={56} weight="duotone" /><Strong>New Customer</Strong>
+								</span>
+							</Flex>
+						</Box>
+					</Link>
+					<Link href={`${baseUrl}/create/invoice`} className="flex-1 min-w-[250px]">
+						<Box className="bg-gradient-to-br from-violet-500 to-orange-300 h-48 rounded-xl overflow-hidden group">
+							<Flex
+								align="center"
+								justify="center"
+								className="w-full h-full bg-black bg-opacity-25 group-hover:bg-opacity-0 transition-all duration-300"
+							>
+								<span className="flex flex-col items-center" >
+									<PlusCircle size={56} weight="duotone" /><Strong>New Invoice</Strong>
+								</span>
+							</Flex>
+						</Box>
+					</Link>
+					<Link href={`${baseUrl}/create/receipt`} className="flex-1 min-w-[250px]">
+						<Box className="bg-gradient-to-br from-neutral-600 via-orange-200 to-stone-700 h-48 rounded-xl overflow-hidden group">
+							<Flex
+								align="center"
+								justify="center"
+								className="w-full h-full bg-black bg-opacity-25 group-hover:bg-opacity-0 transition-all duration-300"
+							>
+								<span className="flex flex-col items-center" >
+									<PlusCircle size={56} weight="duotone" /><Strong>New Receipt</Strong>
+								</span>
+							</Flex>
+						</Box>
+					</Link>
+				</Flex>
+			</Flex>
+		</Container>
+	);
 }
