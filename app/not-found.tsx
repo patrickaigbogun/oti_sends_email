108f1d0251917c2da7a90f808a73b6608f6624b0
youngboypimpin <rjtekdom@gmail.com>
2024-11-20 22:47:46 +0100

    updated not-found page UI

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,26 @@
-import Link from 'next/link'
- 
+'use client';
+
+import { baseUrl } from "@/utils/constants";
+import { House, WarningCircle } from "@phosphor-icons/react";
+import { Heading, Text, Container, Flex, Box, Button, Strong, IconButton } from "@radix-ui/themes";
+import Link from 'next/link';
+
 export default function NotFound() {
-  return (
-    <div>
-      <h2>Not Found</h2>
-      <p>create</p>
-      <Link href="/">Return Home</Link>
-    </div>
-  )
+	return (
+		<Container className="100vh py-5">
+			<Flex direction="column" align="center" justify="center" gap="6" className="h-full">
+				<IconButton color="red" variant="ghost">
+					<WarningCircle size={96} weight="duotone" />
+				</IconButton>
+				<Heading size="8" className="text-center">Not Found</Heading>
+				<Text size="5" className="text-gray-500 text-center mb-6">
+					Oops! The page you're looking for doesn't exist.
+				</Text>
+				<Link href={`${baseUrl}`}><Button size={'3'} variant="classic">
+					<House weight="bold" size={24} /><Strong >Return Home</Strong>
+				</Button>
+				</Link>
+			</Flex>
+		</Container>
+	);
 }
