32aafdc0593b035290b94fcd7632bb8078fbc1c0
youngboypimpin <rjtekdom@gmail.com>
2024-11-26 18:41:56 +0100

    minor

--- app/lib/addInvoice.ts
+++ app/lib/addInvoice.ts
@@ -9,42 +9,61 @@ import { cookies } from 'next/headers';
 export async function AddInvoice(invoiceData: InvoiceData) {
 
 	try {
+
 		// Forward to API route
 		const apiResponse = await fetch(`${baseUrl}/api/add_invoice`, {
+
 			method: 'POST',
 			body: JSON.stringify(invoiceData)
+
 		});
 
 		const result = await apiResponse.json();
 		// Relay API response back to client
 
-		
-    // Set a cookie to pass status back to client
-    if (result.success) {
-		cookies().set('invoiceStatus', 'success', { 
-		  maxAge: 60, 
-		  path: '/' 
-		});
-	  } else {
-		cookies().set('invoiceStatus', 'failed', { 
-		  maxAge: 60, 
-		  path: '/' 
-		});
-	  }
-  
-	  return {
-		success: result.success,
-		message: result.message
-	  };
+
+		// Set a cookie to pass status back to client
+		if (result.success) {
+
+			cookies().set('invoiceStatus', 'success', {
+				maxAge: 60,
+				path: '/'
+
+			});
+
+		} else {
+
+			cookies().set('invoiceStatus', 'failed', {
+				maxAge: 60,
+				path: '/'
+
+			});
+
+		}
+
+		return {
+
+			success: result.success,
+			message: result.message
+
+		};
+
 	} catch (error) {
-	  cookies().set('invoiceStatus', 'failed', { 
-		maxAge: 60, 
-		path: '/' 
-	  });
-	  
-	  return {
-		success: false,
-		message: `Invoice could not be processed: ${error}`
-	  };
+
+		cookies().set('invoiceStatus', 'failed', {
+
+			maxAge: 60,
+			path: '/'
+
+		});
+
+		return {
+
+			success: false,
+			message: `Invoice could not be processed: ${error}`
+
+		};
+
 	}
-  }
+	
+}
